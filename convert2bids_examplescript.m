5438a40b4f1569fa0c9b5b57c971ef3254bfc8c5
c1356674 <magazzinil@cardiff.ac.uk>
2018-04-23 17:55:54 +0100

    update example script

--- convert2bids_examplescript.m
+++ convert2bids_examplescript.m
@@ -7,17 +7,14 @@
 
 %% 1. convert a raw MEG dataset to BIDS format
 
-%specify your own path to "exampledata" directory here
-basepath = '/';
-
 %full path to raw MEG dataset
-rawdataset_dir = fullfile(basepath, '/exampledata/raw/example001');
+rawdataset_dir = '/example/data/raw';
 rawdataset_name = 'example001_nback.ds';
 rawdataset = fullfile(rawdataset_dir, rawdataset_name)
 
 %define BIDS structure fields (see "help rename_meg2bids")
 bids = struct;
-bids.dir = fullfile(basepath, '/exampledata/bids');
+bids.dir = '/example/data/bids';
 bids.participant_label = '001';
 bids.task_label = 'nback';
 
@@ -31,13 +28,13 @@ bids.task_label = 'nback';
 %% 2. convert a NIfTI file to BIDS format
 
 %full path to NIfTI file
-orignifti_dir = fullfile(basepath, '/exampledata/raw/example001');
+orignifti_dir = '/example/data/raw';
 orignifti_name = 'example001.nii';
 orignifti = fullfile(orignifti_dir, orignifti_name)
 
 %define BIDS structure fields (see "help rename_nii2bids")
 bids = struct;
-bids.dir = fullfile(basepath, '/exampledata/bids');
+bids.dir = '/example/data/bids';
 bids.participant_label = '001';
 bids.modality_label = 'T1w';
 
